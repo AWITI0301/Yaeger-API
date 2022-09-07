cbfb382de116610583b19fe6a42a5cccbe03d0ff
AWITI0301 <johnoketchawiti@gmail.com>
2022-09-08 00:00:46 +0300

    add POST,PATCH,DELETE AND GET in games controller

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,61 @@
+class GamesController < ApplicationController
+
+    # GET: /games
+    get "/games" do
+  
+    Game.all.to_json(include: :comments )
+      
+      # return a JSON response with an array of all the game data
+  
+      # Game.all.to_json(include: [user: {except: [:created_at, :updated_at]}])
+    end
+  
+   # POST: /games
+    post "/games" do
+      game= Game.create(
+        name: params[:name],
+        image_url: params[:image_url],
+        user_id: params[:user_id]
+      )
+      if game.id
+        game.to_json
+      else
+        game.errors.full_messages.to_sentence
+        {errors: game.errors.full_messages.to_sentence}.to_json
+      end
+    end
+  
+    # GET: /games/5
+    get "/games/:id" do
+      game = Game.find_by(id: params[:id])
+     if game
+        game.to_json
+      else
+        {errors: "Record not found with id #{params['id']}"}.to_json
+      end
+    end
+  
+     # PATCH: /games/5
+    patch "/games/:id" do
+      game = Game.find(params[:id])
+     if game && game.update(params)
+        game.to_json
+      elsif !game
+        {errors: "Record not found with id #{params['id']}"}.to_json
+      else
+        {errors: game.errors.full_messages.to_sentence}.to_json
+      end
+    end
+  
+    # DELETE: /games/5/delete
+    delete "/games/:id" do
+      game = Game.find_by(id: params[:id])
+      if game&.destroy
+        {messages: "Record successfully destroyed"}.to_json
+      else
+        {errors: "Record not found with id #{params['id']}"}.to_json
+      end
+    end
+  
+   
+  end
